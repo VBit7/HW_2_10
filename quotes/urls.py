b53188b9c4c5237e15da8bd3445590d6e0233631
Vitalii Bielimov <vbit2000@gmail.com>
2024-03-20 22:03:23 +0200

    added the ability to add quotes and authors, added authors catalog

--- quotes/urls.py
+++ quotes/urls.py
@@ -9,12 +9,8 @@ urlpatterns = [
     path('', views.main, name='root'),
     path('<int:page>/', views.main, name='root_paginate'),
     path('author/<int:author_id>/', views.author_detail, name='author_detail'),
+    path('add_author', views.add_author, name='add_author'),
+    path('add_quote', views.add_quote, name='add_quote'),
+    path('authors/', views.authors, name='authors'),
+    path('authors/page/<int:page>/', views.authors, name='authors_paginated'),
 ]
-
-# path('', views.main, name='home'),  # quotes:home
-# path('<int:page>', views.main, name='home_paginate'),
-# path('authors', views.authors, name='authors'),
-# path('authors/page/<int:page>/', views.authors, name='authors_paginated'),
-# path('add_quote', views.add_quote, name='add_quote'),
-# path('add_author', views.add_author, name='add_author'),
-# path('author/<int:author_id>/', views.author_detail, name='author_detail')
--- quotes/views.py
+++ quotes/views.py
@@ -1,20 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 
-from .utils import get_mongodb
-
-# from django.shortcuts import render, redirect
-# from django.core.paginator import Paginator
-# from .forms import AuthorForm, QuoteForm
+from .forms import AuthorForm, QuoteForm
 from .models import Quote, Author
 
 
 def main(request, page=1):
-    quotes = Quote.objects.all().order_by('-created_at')  # Отримуємо всі цитати з бази даних SQLite
+    quotes = Quote.objects.all().order_by('-created_at')
     per_page = 10
     paginator = Paginator(quotes, per_page)
     quotes_on_page = paginator.page(page)
-    # return render(request, 'quotes/index.html', context={'title': 'Home', 'quotes': quotes_on_page})
     return render(request, 'quotes/index.html', context={'quotes': quotes_on_page})
 
 
@@ -22,25 +17,41 @@ def author_detail(request, author_id):
     author = Author.objects.get(pk=author_id)
     return render(request, 'quotes/author_detail.html', {'author': author})
 
-#
-#
-# def authors(request, page=1):
-#     autors = Author.objects.all()  # Отримуємо всіх Автори з бази даних SQLite
-#     per_page = 24
-#     paginator = Paginator(autors, per_page)
-#     authors_on_page = paginator.page(page)
-#     return render(request, 'quotes/authors.html',
-#                   context={'title': 'Autors', 'page': 'autors', 'authors': authors_on_page})
-#
-#
-# def add_author(request):
-#     form = AuthorForm(instance=Author())
-#     if request.method == 'POST':
-#         form = AuthorForm(request.POST, request.FILES, instance=Author())
-#         if form.is_valid():
-#             form.save()
-#             return redirect(to='quotes:home')
-#     return render(request, 'quotes/add_author.html',
-#                   context={'title': 'Add Author', 'page': 'add_author', "form": form})
-#
-#
+
+def add_author(request):
+    form = AuthorForm(instance=Author())
+    if request.method == 'POST':
+        form = AuthorForm(request.POST, request.FILES, instance=Author())
+        if form.is_valid():
+            form.save()
+            return redirect(to='quotes:root')
+    return render(request, 'quotes/add_author.html',
+                  context={'title': 'Add Author', 'page': 'add_author', "form": form})
+
+
+def authors(request, page=1):
+    autors = Author.objects.all()
+    per_page = 24
+    paginator = Paginator(autors, per_page)
+    authors_on_page = paginator.page(page)
+    return render(request, 'quotes/authors.html',
+                  context={'title': 'Autors', 'page': 'autors', 'authors': authors_on_page})
+
+
+def add_quote(request):
+    if request.method == 'POST':
+        form = QuoteForm(request.POST)
+        if form.is_valid():
+            author = form.cleaned_data['author']
+            quote = form.save(commit=False)
+            quote.author = author
+            quote.save()
+            tags = form.cleaned_data['tags']
+            quote.tags.set(tags)
+
+            return redirect(to='quotes:root')
+    else:
+        form = QuoteForm()
+
+    return render(request, 'quotes/add_quote.html', {'form': form})
+
